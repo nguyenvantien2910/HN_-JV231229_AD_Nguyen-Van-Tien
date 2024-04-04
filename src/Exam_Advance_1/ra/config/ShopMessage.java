902b5a288815a4461ee77fd36dae621bf10d72a6
Nguyen Van Tien <nguyenvantien2910@gmail.com>
2024-04-04 15:53:35 +0700

    push hackathon

---
+++ src/Exam_Advance_1/ra/config/ShopMessage.java
@@ -0,0 +1,8 @@
+package Exam_Advance_1.ra.config;
+
+public class ShopMessage {
+        public static String SELECT_INVALID = "Lựa chọn không hợp lệ, mời nhập lại !";
+            public static String EMTY_LIST = "Danh sách trống !";
+            public static String ADD_NEW_SUCESS = "Thêm mới thành công !";
+            public static String NAME_NOT_FOUND = "Tên không tồn tại, mời nhập lại !";
+}
